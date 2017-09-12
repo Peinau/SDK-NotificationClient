e3277907d2ee952f444e1ecc13abb0bad760c003
David Antonio Muñoz Gaete <david.munoz@peanuthub.cl>
2017-09-11 22:14:50 -0300

    support for es5

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.NotificationClient = undefined;
+
+var _NotificationClient = require('./client/NotificationClient');
+
+var _NotificationClient2 = _interopRequireDefault(_NotificationClient);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.NotificationClient = _NotificationClient2.default;
+//# sourceMappingURL=index.js.map

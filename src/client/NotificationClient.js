6c13e0b80432e52380c148323795a7660645ff31
David Antonio Muñoz Gaete <david.munoz@peanuthub.cl>
2017-11-08 00:20:51 -0300

    Add Support to Api Key

--- src/client/NotificationClient.js
+++ src/client/NotificationClient.js
@@ -39,11 +39,12 @@ class NotificationClient {
           if (error) {
             return reject(error);
           }
-          
+
           if (response.statusCode >= 200 && response.statusCode <= 210) {
             resolve(body);
           } else {
-            return reject(response);
+            body.rawResponse = response;
+            return reject(body);
           }
 
         });
